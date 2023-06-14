b161139b52efe540b6b071dce0b2ff94030cfa8d
Arthur Arash Briceno Heidari <124720658+Arthurbric@users.noreply.github.com>
2023-06-14 16:10:55 -0300

    Update app.py
    Arthur apaguei a Função

--- app.py
+++ app.py
@@ -1,43 +1,22 @@
 from flask import Flask, render_template, request
 
-
 app = Flask(__name__)
-
-
-
 
 @app.route('/')
 def login_form():
    return render_template('index.html')
-
-
-
 
 @app.route('/pass', methods=['GET'])
 def process_login():
    user = request.args.get('user')
    senha = request.args.get('senha')
 
-
    # Faça o processamento necessário com os valores recebidos
    # Neste exemplo, vamos apenas retornar os valores dentro de uma função
-
-
-   def login_data(username, password):
-       if username == 'guilherme' and password == '123':
-           return f'Username: {username}, Password: {password}'
-       else:
-           return render_template('index.html')
-
-
-
-
-   result = login_data(user, senha)
-   return result
-
-
-
+   if user == 'Guilherme' and senha == '123':
+       return render_template('pass.html',nome = user)
+   else:
+       return render_template('index.html')
 
 if __name__ == '__main__':
    app.run()
-

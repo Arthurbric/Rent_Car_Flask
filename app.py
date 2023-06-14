b95a0c96b7141513414c97a38a4b154ee67387ac
Arthur Arash Briceno Heidari <124720658+Arthurbric@users.noreply.github.com>
2023-06-14 16:01:37 -0300

    Update app.py
    Guilherme flask update for logging

--- app.py
+++ app.py
@@ -1,18 +1,43 @@
 from flask import Flask, render_template, request
 
+
 app = Flask(__name__)
 
+
+
+
 @app.route('/')
-def index():
-  return render_template('index.html')
+def login_form():
+   return render_template('index.html')
+
+
+
 
 @app.route('/pass', methods=['GET'])
-def check():
+def process_login():
+   user = request.args.get('user')
+   senha = request.args.get('senha')
 
-  nome=str(request.form['user'])
-  cpf=str(request.form['senha'])
 
-  if nome == "Arthur" or cpf == "1234" :
-    return render_template('pass.html', nome)
-  return render_template('pass.html')
-app.run(debug=1)
+   # Faça o processamento necessário com os valores recebidos
+   # Neste exemplo, vamos apenas retornar os valores dentro de uma função
+
+
+   def login_data(username, password):
+       if username == 'guilherme' and password == '123':
+           return f'Username: {username}, Password: {password}'
+       else:
+           return render_template('index.html')
+
+
+
+
+   result = login_data(user, senha)
+   return result
+
+
+
+
+if __name__ == '__main__':
+   app.run()
+
